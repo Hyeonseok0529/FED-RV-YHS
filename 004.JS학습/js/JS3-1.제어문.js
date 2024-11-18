3e2968af9e5309a47c4381f4ab1bdd21790fd6d8
Hyeonseok Yang <gustjr8293@naver.com>
2024-11-18 10:50:58 +0900

    자바스크립트

--- 004.JS학습/js/JS3-1.제어문.js
+++ 004.JS학습/js/JS3-1.제어문.js
@@ -444,3 +444,31 @@ function showLocal() {
       없으면 이 부분이 실행됨(단, 필요시 사용)
       -> default문에는 break를 쓰지 않는다!
   **********************************************/
+
+
+  // [for문 구구단 출력을 위한 코드 ] //
+  // 1. 대상선정 - 이벤트대상, 변경대상
+  // 1-1. 이벤트 대상 : select#selbx
+  var selbx = document.querySelectorAll("#selbx");
+  // 1-2. 변경 대상 : 출력대상 - .g1
+  var g1 = document.querySelectorAll(".g1");
+  // DOM 선택이 된 이유는 외부JS호출시 
+  // defer로 호출했기 떄문에
+  // html태그 로딩후 실행됨!
+  // console.log("선택대상:",selbx,g1);
+
+  // 2. 이벤트 설정하기 //
+  selbx.onchange = makeGugu;
+  // 함수를 그대로 할당하면 이벤트 발생시 함수를 실행함!
+
+  // 3. 함수 만들기 // 
+  function makeGugu(){
+    // 함수를 호출한 요소 자신은? this!
+
+    // 1. 선택값 option의 value값
+    var optVal = this.value;
+
+    // 1. 함수호출확인
+    console.log("구구단을 쓰자!", optVal);
+
+  } ////////// makeGugu //////////
