e319951523eb420818d6d02b400728fc119fc46c
Hyeonseok Yang <gustjr8293@naver.com>
2024-11-18 10:56:29 +0900

    Update JS3-1.제어문.js

--- 004.JS학습/js/JS3-1.제어문.js
+++ 004.JS학습/js/JS3-1.제어문.js
@@ -468,7 +468,14 @@ function showLocal() {
     // 1. 선택값 option의 value값
     var optVal = this.value;
 
-    // 1. 함수호출확인
-    console.log("구구단을 쓰자!", optVal);
+    // 2. 함수호출확인
+    console.log("구구단을 쓰자!", optVal, this);
+
+    // 3. 구구단 만들기
+    var hcode = `<h2>${optVal}단</h2>`;
+
+    // 4. 화면요소에 출력하기 //
+    // 출력대상은 g1변수 -> .g1박스
+    g1.innerHTML = hcode;
 
   } ////////// makeGugu //////////
