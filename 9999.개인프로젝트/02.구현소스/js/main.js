c152b2219a4ee97683e398c9ef49c3b99f239db4
Hyeonseok Yang <gustjr8293@naver.com>
2024-12-17 17:54:13 +0900

    피드백 후 소스 업뎃!

---
+++ 9999.개인프로젝트/02.구현소스/js/main.js
@@ -0,0 +1,17 @@
+// 메인 JS
+
+$('.hambtn').click(function(e){
+    e.preventDefault(); 
+    let isOn = $(this).is('.on');
+    $('.smenu').css({translate: isOn?'0':'0 -170%'});
+    $('body').toggleClass('on');
+})
+
+const $cartBox = $('.minicart');
+$('.right-menu').click(function(e){
+    e.preventDefault(); 
+    let isOn = $cartBox.css('translate');
+    console.log(isOn);
+    $cartBox.css({translate: isOn=='100%'?'0':'100%'});
+    $('body').toggleClass('on');
+})
