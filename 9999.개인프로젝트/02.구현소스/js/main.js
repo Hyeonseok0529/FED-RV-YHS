b9de6d27582ad410c194e865b8fb3d94f2497b46
Hyeonseok Yang <gustjr8293@naver.com>
2024-12-20 16:15:25 +0900

    소스 피드백 완료

--- 9999.개인프로젝트/02.구현소스/js/main.js
+++ 9999.개인프로젝트/02.구현소스/js/main.js
@@ -1,18 +1,24 @@
 // 메인 JS
 
-$('.hambtn').click(function(e){
-    e.preventDefault(); 
-    e.stopPropagation();
-    let isOn = $(this).is('.on');
-    $('.smenu').css({translate: isOn?'0':'0 -220%'});
-    $('body').toggleClass('on');
-})
+$(".hambtn").click(function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  let isOn = $(this).is(".on");
+  $(".smenu").css({ translate: isOn ? "0" : "0 -220%" });
+  $("body").toggleClass("on");
+});
 
-const $cartBox = $('.minicart');
-$('.right-menu').click(function(e){
-    e.preventDefault(); 
-    let isOn = $cartBox.css('translate');
-    console.log(isOn);
-    $cartBox.css({translate: isOn=='100%'?'0':'100%'});
-    $('body').toggleClass('on');
-})
+const $cartBox = $(".minicart");
+$(".right-menu").click(function (e) {
+  e.preventDefault();
+  let isOn = $cartBox.css("translate");
+  console.log(isOn);
+  $cartBox.css({ translate: isOn == "100%" ? "0" : "100%" });
+  $("body").toggleClass("on");
+});
+
+$(".smenu a").click((e) => {
+  let txt = $(e.currentTarget).text();
+  if (txt == "HOME") location.href = "index.html";
+  else if (txt == "LOOKBOOK") location.href = "sub-lookbook.html";
+});
