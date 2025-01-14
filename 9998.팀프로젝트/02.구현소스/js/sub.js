08878644ef4ecf653ebefa80e4ac4cda9aa3dc7e
Hyeonseok Yang <gustjr8293@naver.com>
2025-01-14 17:43:02 +0900

    갤러리 이미지 데이터 코딩하는 중

--- 9998.팀프로젝트/02.구현소스/js/sub.js
+++ 9998.팀프로젝트/02.구현소스/js/sub.js
@@ -1,1 +1,11 @@
 // sub-js //
+
+// 갤러리 사진클릭시 원본보기//
+// 이벤트 대상 : .gallery > div
+$('.gallery>div').click(function(){
+console.log("미리보기!")
+
+// 1. 클릭된 박스의 데이터 읽어오기
+let currImg = $(this).find("img").html();
+
+}) ///// click /////
