686d3a561c7157550c7b2110ca768f047cbc873e
Hyeonseok Yang <gustjr8293@naver.com>
2025-01-14 16:35:03 +0900

    컴포넌트

--- 007.VueJS학습/02.기초연습/js/comp_real.js
+++ 007.VueJS학습/02.기초연습/js/comp_real.js
@@ -1,4 +1,8 @@
-// 01. 컴포넌트 연습 JS
+// 02. 컴포넌트 현실화 JS - comp_real.js
+
+// 새로운 리스트 데이터 불러오기
+import deData from './comp_data.json' with{type: 'json'};
+console.log(deData);
 
 // 1. 상단영역 전역 컴포넌트 만들기
 // Vue.component(컴포넌트명,{template:코드})
@@ -13,7 +17,7 @@ Vue.component("tit-comp", {
     `,
 }); ///// 전역컴포넌트1 /////
 
-new Vue({el:'.tit'});
+// new Vue({el:'.tit'});
 
 
 // [갤러리용 변수]
@@ -44,7 +48,7 @@ Vue.component('list-comp',{
     // 부모가 공개한 바인딩 속성을 가져온다!
     // 프롭스 다운!!! -> 부모요소에 만든 요소명을 등록함!
     // props: [] -> 배열형태로 여러개 등록 가능!
-    props:['list-num','my-seq'],
+    props:['list-num','my-seq','fn-add-comma'],
     // 주의: 이것을 변수로 쓸때는 캐밥케이스를 캐믈케이스로 
     // 바꿔서 쓴다~! 예) 'list-num' -> listNum
     // 그리고 프롭스 다운변수도 내부에 등록되었으므로
@@ -68,7 +72,7 @@ Vue.component('list-comp',{
             //   일반데이터로 사용할 수 없다! 에러남!
 
             // 상품가격
-            gprice: this.addComma(123000*this.listNum/2)+`원`,
+            gprice: this.fnAddComma(123000 * this.listNum/2)+`원`,
         }
     },// data속성
 
@@ -77,11 +81,6 @@ Vue.component('list-comp',{
         // 연속번호만들기 테스트용 메서드
         setNum(){
             return ++inum;
-        },
-        // 세자리마다 콤마추가 메서드
-        addComma(x){
-            return x.toString()
-            .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         },
         // 부모와 자식 컴포넌트 연결하기
         goPapa(txt){//-> goPapa는 자식컴포넌트에서 호출!
@@ -96,19 +95,19 @@ Vue.component('list-comp',{
 });
 
 // 컴포넌트의 부모 뷰인스턴스
-new Vue({
-    el:'.grid',
-    // 자식컴포넌트의 전달값을 받기위한 메서드를 만든다!
-    methods:{
-        // 자식이벤트 전달후 실행메서드
-        goMsg(txt){// txt 전달받을 변수
-            alert('자식이 부모에게 이벤트전달 성공!'+txt);
-        },
-        ovMsg(obj){// -> ovNow도 자식 컴포넌트에서 호출!
-           console.log('오버!오케이!',obj);
-        }
-    },
-});
+// new Vue({
+//     el:'.grid',
+//     // 자식컴포넌트의 전달값을 받기위한 메서드를 만든다!
+//     methods:{
+//         // 자식이벤트 전달후 실행메서드
+//         goMsg(txt){// txt 전달받을 변수
+//             alert('자식이 부모에게 이벤트전달 성공!'+txt);
+//         },
+//         ovMsg(obj){// -> ovNow도 자식 컴포넌트에서 호출!
+//            console.log('오버!오케이!',obj);
+//         }
+//     },
+// });
 
 
 // 3. 유튜브 동영상 컴포넌트 만들기
@@ -118,16 +117,21 @@ Vue.component("ifr-comp",{
     <iframe width="49%" style="aspect-ratio: 16/9;" 
     v-bind:src="ifrSrc" title="#고윤정 과 함께 차가운 겨울을 더욱 액티브하게!  l 디스커버리 23FW #goyounjung #크롭패딩" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> 
     `, /// template ////
-    // 3-2. data 옵션
+
+    //3-2. 프롭스다운!
+    props: ["mv-code"],
+    //mv-code는 동영상 아이디
+    
+    // 3-3. data 옵션
     data(){
         return{
-            ifrSrc: `https://www.youtube.com/embed/ZH1Y1l1OmTY?autoplay=1&mute=1&loop=1&playlist=ZH1Y1l1OmTY`,
+            ifrSrc: `https://www.youtube.com/embed/${this.mvCode}?autoplay=1&mute=1&loop=1&playlist=${this.mvCode}`,
         };
     },
 });
 
 // 뷰인스턴스 생성하기 : 유튜브 동영상 컴포넌트
-new Vue({el:".you-box"});
+// new Vue({el:".you-box"});
 
 // 4. 하단 컴포넌트 만들기
 Vue.component("footer-comp",{
@@ -140,4 +144,66 @@ Vue.component("footer-comp",{
 });
 
 // 뷰인스턴스 생성하기 : 하단 컴포넌트
-new Vue({el:".tit2"});
+// new Vue({el:".tit2"});
+
+
+// 모든 컴포넌트는 모두 뷰인스턴스 생성전에 셋팅하면 된다!
+// 새로운 리스트 컴포넌트 셋팅하기 //
+Vue.component('de-fashion-list',{
+    template:`
+        <div>
+            <img v-bind:src="gsrc" alt="아이템">
+            <aside>
+                <h2>{{gname}}</h2>
+                <h3>{{gprice}}</h3>
+            </aside>
+        </div>
+    `, ////// template ////
+
+    // 2. 프롭스 다운!
+    props:['list-idx','list-tit','list-price','fn-add-comma',
+        //케밥 케이스를 케믈케이스로 변경하여 변수/함수로 사용가능!
+        //listIdx, listTit, listPrice, fnAddComma 로 사용가능하다는 말
+        // 실제로 사용할 때는 this키워드를 사용하여 객체내부용으로 씀.
+,    ],
+    // 3. 데이터 : 컴포넌트 데이터는 리턴함수형태로 해야함!
+    data(){
+        return{
+            gsrc:`./images/discovery/de_${this.listIdx}.jpg`,
+            gname: this.listTit,
+            gprice:this.fnAddComma(this.listPrice)+"원",
+        }
+    },
+})
+
+
+//////////////////////////////////////////////////////////////////////////
+///////////////// 전체 최상위 부모인 .main-wrap을 //////////////////////////
+///////////////// 유일한 뷰 인스턴스로 생성함!//////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+new Vue({
+    el:'.main-wrap',
+    // 부모 뷰인스턴스 데이터구역 //
+    data:{
+        // 부모 컴포넌트에서 사용할 import한 데이터 할당!
+        exData: deData,
+        // 하위 컴포넌트를 데이터만큼 순회할 때 여기서 불러온다!
+        // 부모 인스턴스에서 v-for 디렉티브를 사용하기 때문!
+    },
+    // 부모 뷰인스턴스 메서드구역 //
+    methods:{
+        // 자식이벤트 전달후 실행메서드
+        goMsg(txt){// txt 전달받을 변수
+            alert('자식이 부모에게 이벤트전달 성공!'+txt);
+        },
+        ovMsg(obj){// -> ovNow도 자식 컴포넌트에서 호출!
+           console.log('오버!오케이!',obj);
+        },
+        // 세자리마다 콤마추가 메서드
+        addComma(x){
+            return x.toString()
+            .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        },
+        
+    },
+});
