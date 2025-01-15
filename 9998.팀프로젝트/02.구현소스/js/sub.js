6f645d2bb021632fbe5c60f7073e382eeec386ee
Hyeonseok Yang <gustjr8293@naver.com>
2025-01-16 07:47:49 +0900

    이미지 뿌리기 성공!

--- 9998.팀프로젝트/02.구현소스/js/sub.js
+++ 9998.팀프로젝트/02.구현소스/js/sub.js
@@ -3,7 +3,7 @@
 // 갤러리 사진클릭시 원본보기//
 // 이벤트 대상 : .gallery > div
 // 변경대상: .smenu-contbox img
-const $smenuImg = $(".smenu-contbox img");
+const $smenuImg = $(".smenu-contbox");
 $('.gallery img').click(function(){
 // console.log("미리보기!")
 
@@ -12,5 +12,6 @@ let currImg = $(this).attr("src");/* .find(''); */
 console.log(currImg)
 
 // 2. 읽어온 내용을 서브 컨텐츠 박스에 넣기
-$smenuImg.find('div').html(currImg);
+$smenuImg.html(`<img src="${currImg}" alt="이미지"></img>`);
+
 }) ///// click /////
