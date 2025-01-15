f8bdaaf53d90914c18c0d70b08716b0c041c5971
Hyeonseok Yang <gustjr8293@naver.com>
2025-01-15 17:54:13 +0900

    이미지 데이터 읽어오기 완료

--- 9998.팀프로젝트/02.구현소스/js/sub.js
+++ 9998.팀프로젝트/02.구현소스/js/sub.js
@@ -2,10 +2,15 @@
 
 // 갤러리 사진클릭시 원본보기//
 // 이벤트 대상 : .gallery > div
-$('.gallery>div').click(function(){
-console.log("미리보기!")
+// 변경대상: .smenu-contbox img
+const $smenuImg = $(".smenu-contbox img");
+$('.gallery img').click(function(){
+// console.log("미리보기!")
 
-// 1. 클릭된 박스의 데이터 읽어오기
-let currImg = $(this).find("img").html();
+// 1. 클릭된 박스의 이미지 읽어오기
+let currImg = $(this).attr("src");/* .find(''); */
+console.log(currImg)
 
+// 2. 읽어온 내용을 서브 컨텐츠 박스에 넣기
+$smenuImg.find('div').html(currImg);
 }) ///// click /////
