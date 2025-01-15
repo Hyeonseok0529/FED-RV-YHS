26303419920451a7ae4ec9cf886ed71f43f6323f
Hyeonseok Yang <gustjr8293@naver.com>
2025-01-16 08:33:44 +0900

    서브메뉴 띄우기 성공!
    닫히는 기능만 구현하면 됨!

--- 9998.팀프로젝트/02.구현소스/js/sub.js
+++ 9998.팀프로젝트/02.구현소스/js/sub.js
@@ -4,6 +4,7 @@
 // 이벤트 대상 : .gallery > div
 // 변경대상: .smenu-contbox img
 const $smenuImg = $(".smenu-contbox");
+const $smenuImgbox = $(".smenu-imgbox");
 $('.gallery img').click(function(){
 // console.log("미리보기!")
 
@@ -12,6 +13,6 @@ let currImg = $(this).attr("src");/* .find(''); */
 console.log(currImg)
 
 // 2. 읽어온 내용을 서브 컨텐츠 박스에 넣기
-$smenuImg.html(`<img src="${currImg}" alt="이미지"></img>`);
+$smenuImgbox.html(`<img src="${currImg}" alt="이미지"></img>`);
 
 }) ///// click /////
