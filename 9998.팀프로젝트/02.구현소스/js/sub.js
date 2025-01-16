f07a93980bfa9fdf57b476b06d1f76357ed4bc4e
Hyeonseok Yang <gustjr8293@naver.com>
2025-01-16 11:42:15 +0900

    갤러리 서브메뉴 기능 구현 완료
    서브메뉴 사진 미디어 쿼리 진행해야함

--- 9998.팀프로젝트/02.구현소스/js/sub.js
+++ 9998.팀프로젝트/02.구현소스/js/sub.js
@@ -1,13 +1,14 @@
 // sub-js //
 
+
 // 갤러리 사진클릭시 원본보기//
 // 이벤트 대상 : .gallery > div
 // 변경대상: .smenu-contbox img
 const $smenuImg = $(".smenu-contbox");
 const $smenuImgbox = $(".smenu-imgbox");
+const $gallerySmenu = $(".gallery-smenu");
+const $closeBtn = $(".close");
 
-// 닫기버튼 셋팅
-// $(".close").click(() => $(".gallery-smenu").fadeOut());
 
 $('.gallery img').click(function(){
 // console.log("미리보기!")
@@ -17,6 +18,15 @@ let currImg = $(this).attr("src");/* .find(''); */
 console.log(currImg)
 
 // 2. 읽어온 내용을 서브 컨텐츠 박스에 넣기
-$smenuImgbox.html(`<img src="${currImg}" alt="이미지"></img>`);
+$smenuImgbox.html(`<img src="${currImg}" alt="이미지">`);
 
+// 3. 상세이미지 보기 박스 보이기
+$gallerySmenu.fadeIn(500);
 }) ///// click /////
+
+// 닫기버튼 셋팅
+// $(".close").click(() => $(".gallery-smenu").fadeOut());
+// -> 버튼을 변수에 할당해서 이벤트를 설정하기!
+$closeBtn.click(()=>$gallerySmenu.fadeOut(500));
+
+// 타이핑 애니메이션 셋팅 // 
