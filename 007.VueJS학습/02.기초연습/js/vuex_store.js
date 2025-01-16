21e67b298ab7dbb80e5fa197eca6d82f3c05e910
Hyeonseok Yang <gustjr8293@naver.com>
2025-01-16 15:17:27 +0900

    뷰엑스 스토어

---
+++ 007.VueJS학습/02.기초연습/js/vuex_store.js
@@ -0,0 +1,70 @@
+// 04. 뷰엑스 스토아 - 저장공간 활용하기 JS
+
+// [1] 전역 컴포넌트 만들기 //
+// (1) 상단영역 컴포넌트
+Vue.component('top-area',{
+    // 템플릿 설정
+    template: `
+        <header>
+            <ul class="gnb">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+            </ul>
+        </header>
+    `,
+    // 데이터 설정 : 컴포넌트는 반드시 리턴을 하는 객체(객체리턴메서드)를 써야함!
+    data(){
+        return{};
+    } ,
+    // 메서드 설정
+    methods:{},
+});
+// (2) 메인영역 컴포넌트
+Vue.component('main-area',{
+    // 템플릿 설정
+    template: `
+        <main>
+            <img src="https://img.freepik.com/premium-vector/city-illustration_23-2147514701.jpg" alt="지역이미지" />
+
+            <p>
+            도시소개에 오신것을 환영합니다!
+            </p>
+        </main>
+    `,
+    // 데이터 설정 : 컴포넌트는 반드시 리턴을 하는 객체(객체리턴메서드)를 써야함!
+    data(){
+        return{};
+    } ,
+    // 메서드 설정
+    methods:{},
+});
+// (3) 하단영역 컴포넌트
+Vue.component('info-area',{
+    // 템플릿 설정
+    template: `
+    <footer>
+            <address>
+                서울시 강남구 역삼동 119 뷰엑스B
+            </address>
+        </footer>
+    `,
+    // 데이터 설정 : 컴포넌트는 반드시 리턴을 하는 객체(객체리턴메서드)를 써야함!
+    data(){
+        return{};
+    } ,
+    // 메서드 설정
+    methods:{},
+});
+
+// [2] 뷰 인스턴스 생성하기 //
+// 대상요소 : #app
+new Vue({
+    // 대상선정
+    el: "#app",
+    // 데이터
+    data:{},
+    // 메서드 
+    methods:{},
+    
+});
