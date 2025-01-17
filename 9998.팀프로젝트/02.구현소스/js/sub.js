7063995d667682f9679347af9df4f68d373b768c
Hyeonseok Yang <gustjr8293@naver.com>
2025-01-17 09:51:38 +0900

    계획서 , 코드 소스 업ㄷ세

--- 9998.팀프로젝트/02.구현소스/js/sub.js
+++ 9998.팀프로젝트/02.구현소스/js/sub.js
@@ -1,6 +1,5 @@
 // sub-js //
 
-
 // 갤러리 사진클릭시 원본보기//
 // 이벤트 대상 : .gallery > div
 // 변경대상: .smenu-contbox img
@@ -9,6 +8,8 @@ const $smenuImgbox = $(".smenu-imgbox");
 const $gallerySmenu = $(".gallery-smenu");
 const $closeBtn = $(".close");
 
+// 닫기버튼 셋팅
+// $(".close").click(() => $(".gallery-smenu").fadeOut());
 
 $('.gallery img').click(function(){
 // console.log("미리보기!")
@@ -21,12 +22,7 @@ console.log(currImg)
 $smenuImgbox.html(`<img src="${currImg}" alt="이미지">`);
 
 // 3. 상세이미지 보기 박스 보이기
-$gallerySmenu.fadeIn(500);
+$gallerySmenu.fadeIn(300);
 }) ///// click /////
 
-// 닫기버튼 셋팅
-// $(".close").click(() => $(".gallery-smenu").fadeOut());
-// -> 버튼을 변수에 할당해서 이벤트를 설정하기!
-$closeBtn.click(()=>$gallerySmenu.fadeOut(500));
-
-// 타이핑 애니메이션 셋팅 // 
+$closeBtn.click(()=>$gallerySmenu.fadeOut(300));
