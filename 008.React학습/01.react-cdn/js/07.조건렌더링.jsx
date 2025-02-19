c743f5b3cd28f9834b535b2aa9bbad74bac5bdbe
Hyeonseok Yang <gustjr8293@naver.com>
2025-02-19 11:21:42 +0900

    Update 07.조건렌더링.jsx

--- 008.React학습/01.react-cdn/js/07.조건렌더링.jsx
+++ 008.React학습/01.react-cdn/js/07.조건렌더링.jsx
@@ -180,41 +180,67 @@ function MakeList({foodName,movieInfo}){
 } ///// MakeList 컴포넌트 /////
 
 // 2-3. 개발자 선호음식 리스트 출력 컴포넌트 //
-function WishList({wList}){ // 변수는 대문자로 시작하면 안됨!
+function WishList({wList, gubun}){ // 변수는 대문자로 시작하면 안됨!
   // wList - 좋아하는 음식 리스트(배열)
+  // gubun - 음식/영화 구분 문자값
   return (
     <React.Fragment>
       {/* 음식 리스트 타이틀 */}
-      <SetTitle title="음식"/>
+      <SetTitle title={gubun}/>
       { // 표현식 코드 구역의 태그는 부모가 하나여야한다 //
       // 배열값이 0개 이상인 경우 출력
       wList.length > 0 &&
         <div>
+          {// 구분값이 "음식"인 경우
+        gubun == "음식" &&
         <h2>
         개발자가 좋아하는 음식은 모두 {wList.length}가지 입니다.
       </h2>
+          }
+          {// 구분값이 "영화"인 경우
+        gubun == "영화" &&
+        <h2>
+        개발자가 좋아하는 영화는 최근 {wList.length}년간 아래와 같습니다.
+      </h2>
+          }
       <ul>
-        {
+        { // 구분값이 "음식"인 경우
+        gubun == "음식" &&
           wList.map(v=>
           <MakeList foodName={v}/>)
-
-          /*배열변수.map() 메서드사용!
-          map메서드는 원래 새로운배열을
-          현재 자리에 출력하는 용도임
-          그러나 리액트는 이것을 변경하여
-          표현식안에서 출력시
-          태그JSX 형식으로 변환해 줌!
-          JS 처럼 map().join('')처리
-          불필요!!!*/
+        }
+        { // 구분값이 "영화"인 경우
+        gubun == "영화" &&
+          wList.map(v=>
+          <MakeList movieInfo={v}/>)
         }
       </ul>
+        { // 구분값이 "영화"인 경우 포스터 출력
+        gubun == "영화" &&
+        wList.map(v=>
+          <MakeImage 
+          isrc={v.poster}
+          ialt={v.mtit}
+          icss={{width:"100px"}}
+          />
+        )
+        }
       </div>
+
+      /*배열변수.map() 메서드사용!
+      map메서드는 원래 새로운배열을
+      현재 자리에 출력하는 용도임
+      그러나 리액트는 이것을 변경하여
+      표현식안에서 출력시
+      태그JSX 형식으로 변환해 줌!
+      JS 처럼 map().join('')처리
+      불필요!!!*/
       }
 
       {/* 배열개수가 0인 경우 다른 것 출력하기 */}
       {
         wList.length == 0 && (
-          <h2> 개발자 음식 리스트가 업데이트 되지 않았습니다.</h2>
+          <h2> 개발자 {gubun} 리스트가 업데이트 되지 않았습니다.</h2>
         )
       }
 
@@ -223,7 +249,27 @@ function WishList({wList}){ // 변수는 대문자로 시작하면 안됨!
 } //// WishList 컴포넌트 /////
 
 // 음식 배열값이 있는 경우 출력하기
-ReactDOM.render(<WishList wList={foods} />, root[2]);
+ReactDOM.render(<WishList wList={foods} gubun="음식"/>, root[2]);
 
 // 음식 배열값이 없는 경우 출력하기
-ReactDOM.render(<WishList wList={[]} />, root[3]);
+ReactDOM.render(<WishList wList={[]} gubun="음식" />, root[3]);
+
+/* 
+[ 출력형태 ]
+    👨‍🔧개발자👩‍🔧가 좋아하는 영화
+    개발자가 좋아하는 영화는 최근 몇년간 아래와 같습니다!
+    2021년도 영화1
+    2022년도 영화2
+    2023년도 영화3
+    ... 여기는 영화포스트 나열하기 ...
+*/
+
+// 제목,리스트,위시리스트 컴포넌트 모두 재활용 //
+
+// 영화 위시리스트 출력하기 : 배열값이 있는 경우 //
+ReactDOM.render(
+<WishList wList={movs} gubun="영화 "/>,root[4]);
+
+// 영화 위시리스트 출력하기 : 배열값이 없는 경우 //
+ReactDOM.render(
+<WishList wList={[]} gubun="영화 "/>,root[5]);
