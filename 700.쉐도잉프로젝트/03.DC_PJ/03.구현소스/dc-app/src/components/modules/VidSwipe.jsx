71d048ed4f94db14fe8cc91a9abd727cc5c6347c
Hyeonseok Yang <gustjr8293@naver.com>
2025-02-28 15:59:29 +0900

    쉐도잉 DC

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/modules/VidSwipe.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/modules/VidSwipe.jsx
@@ -19,13 +19,13 @@ function VidSwipe({ catName }) {
       <h2 className="tit">{catTit[catName]}</h2>
       {/* 2. 스와이퍼 컴포넌트 : SwiperVid
       -> 전달속성 catName으로 데이터선택값 보내기 */}
-      <SwiperVid catName="main" />
+      <SwiperVid catName={catName} />
       {/* 3. 비디오 재생창 */}
       <section className="vid-bx">
         {/* 비디오 중앙박스 */}
         <div className="play-vid">
           {/* 비디오 타이틀 */}
-          <h2 className="ifr-tit">BLUE BEETLE - OFFICIAL TRAILER</h2>
+          <h2 className="ifr-tit"></h2>
           {/* 아이프레임 */}
           <iframe src="" allow="autoplay"></iframe>
           {/* 닫기버튼 */}
