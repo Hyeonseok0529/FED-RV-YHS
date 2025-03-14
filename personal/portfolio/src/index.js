70594e486a8a2d95e9805c4716d76852e7b62ec0
Hyeonseok Yang <gustjr8293@naver.com>
2025-03-14 11:40:43 +0900

    tailwind 설치 완료!

--- personal/portfolio/src/index.js
+++ personal/portfolio/src/index.js
@@ -1,10 +1,8 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import {ThemeProvider} from "styled-components";
-import {theme} from "./theme"
 import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
